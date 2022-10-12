20862807675293478aa33de3c44d8aed416cfc74
JUPUDI  KALYAN <37042639+JUPUDI-VAMSI-KALYAN@users.noreply.github.com>
2022-10-12 19:37:05 +0530

    Update db.py

--- config/db.py
+++ config/db.py
@@ -2,7 +2,7 @@ from time import monotonic
 from pymongo import MongoClient
 
 # conn = MongoClient()
-conn = MongoClient("mongodb://kalyan123:kalyan1234@cluster0.nipgpbz.mongodb.net/?retryWrites=true&w=majority&tls=fals&ssl=false")
+conn = MongoClient("mongodb://kalyan123:kalyan1234@cluster0.nipgpbz.mongodb.net/?retryWrites=true&w=majority")
 db = conn.querybox
 # for e in db["queries"].find():
 #     print(e)
